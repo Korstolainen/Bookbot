0a0c052aa9f7df561eeab2496b05a29786b26aff
Korstolainen <arska2@gmail.com>
2025-05-19 22:58:41 +0300

    added sorting function and editet print commands

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from stats import get_book_text
 from stats import counting_words
 from stats import counting_letters
+from stats import sorting_dictionary_count
 
 def main():
-    book_contents = get_book_text("books/frankenstein.txt")
+    link_to_book = "books/frankenstein.txt"
+    book_contents = get_book_text(link_to_book)
     word_count = counting_words(book_contents)
     letter_count = counting_letters(book_contents)
-    print(f"{word_count} words found in the document")
-    print(letter_count)
+    sorted_letter_count = sorting_dictionary_count(letter_count)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {link_to_book}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for letter_dict in sorted_letter_count:
+        char = letter_dict["char"]  # Get the character from the dictionary
+        if char.isalpha():  # Check if it's alphabetical
+            num = letter_dict["num"]  # Get the count
+            print(f"{char}: {num}")  # Print in required format
+
+    print("============= END ===============")
+
+
     return
 
 main()
--- stats.py
+++ stats.py
@@ -11,9 +11,19 @@ def counting_words(text):#-------------------Takes a string as input. Counts the
         count = count + 1          #counting each word
     return count
 
-def counting_letters(text):#-----------------Takes a string as input. Turns all letters to lowercase. Counts each use of a letter. Returns a dictionary of each letter and how many times it has been used.
+def counting_letters(text):#-----------------Takes a string as input. Turns all letters to lowercase. Counts each use of a letter. Returns a dictionary of each letter as key and how many times the letter has been used.
     count = {}
     text_lowercase = text.lower()
     for char in text_lowercase:
-        count[char] = count.get(char, 0) + 1 #count.get(char, 0) +1 will check if the character is on the list, if it is it will add 1 to its count, if not it will create the dictionary key for the character, set its count to 0 and add 1
+        count[char] = count.get(char, 0) + 1 #Will check if the character is on the list, if it is, it will add 1 to its count, if not it will create the dictionary key for the character, set its count to 0 and add 1
     return count
+
+
+def sorting_dictionary_count(dict):#----------Takes a dictionary, turns that in to a list of dictionaries where {"char": "b", "num": 4868}
+    dict_list = []
+    for key in dict:
+        dict_list.append({"char": key, "num" : dict[key]})#Iterate trought the dictionary and create a list of dictionaries
+    
+    dict_list.sort(key=lambda d: d["num"], reverse=True)#Sort the list of dictionaries. (lambda d: d["num"] is jut a quick way to create a fucntion since the key parameter imputs a function)
+
+    return dict_list
