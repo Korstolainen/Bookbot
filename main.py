8ef7ed3854f689f81acf65eb5bf166e6872b7727
Korstolainen <arska2@gmail.com>
2025-05-19 19:39:15 +0300

    Added a function to get the book

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(path_to_book):
+    book = None
+    with open(path_to_book) as text:
+        book = text.read()
+    return book
+
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+    return
+
+main()
