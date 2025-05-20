94e23a629ff84dc277f777b32f8da3d3d9b7fbb0
Korstolainen <arska2@gmail.com>
2025-05-20 21:59:49 +0300

    added the ability for user to set a path to book

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import sys
 from stats import get_book_text
 from stats import counting_words
 from stats import counting_letters
 from stats import sorting_dictionary_count
 
 def main():
-    link_to_book = "books/frankenstein.txt"
+    argv = sys.argv#-------------get sys.argv which is a list based on the command line (python3 main.py = ['main.py'])(python3 main.py path/to/text.txt = ['main.py', 'path/to/text.txt'])
+    if len(argv) >= 2:#----------if path to book was inputed in the command line
+        link_to_book = argv[1]#--create a variable for the path
+    else:#-----------------------else give user instructions on how to use the program and return the program
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+
+
+
+
     book_contents = get_book_text(link_to_book)
     word_count = counting_words(book_contents)
     letter_count = counting_letters(book_contents)
